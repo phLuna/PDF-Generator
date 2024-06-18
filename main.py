7ecf663f600d5ae7081b29d2c12600766b58c251
Luna. <lunissnyan11@gmail.com>
2024-06-18 14:57:49 -0300

    Quase pronto. Micro ajustes feitos xD

--- main.py
+++ main.py
@@ -47,12 +47,11 @@ def gerar_pdf(
     doc = SimpleDocTemplate(nome_arquivo, pagesize=A4)
     elements = []
     
-    #Estilos de texto
+    #Estilos de texto.
     estilos = getSampleStyleSheet()
     estilo_titulo = estilos['Title']
-    estilo_subtitulo = estilos['Normal']
 
-    #Cabeçalho com título e cinco informações
+    #Título.
     titulo = "Relatório de gerenciamento de chamados"
     paragrafo_titulo = Paragraph(titulo, estilo_titulo)
     elements.append(paragrafo_titulo)
@@ -64,21 +63,21 @@ def gerar_pdf(
             tabela.setStyle(estilo)
         return tabela
 
-    #Cabeçalho do cliente.
+    #Cabeçalho/tabela do cliente.
     dados_cliente = [[f'Cliente: {nome_cliente}'], ['Horas Mensais:', f'{horas_mensais} horas mensais, {horas_trimestrais} horas trimestrais.'], ['Valor mensal:', f'R${valor_mensal}'], ['Valor hora:', f'R${valor_hora}',], ['Valor hora adicional:', f'R${valor_hora_adicional_cliente}']]
     estilo_cliente = TableStyle([('SPAN', (0, 0), (-1, 0)), ('BACKGROUND', (0, 0), (-1, 0), colors.grey), ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke), ('ALIGN', (0, 0), (-1, -1), 'CENTER'), ('GRID', (0, 0), (-1, -1), 1, colors.black)])
     
     elements.append(criar_tabela(dados_cliente, estilo_cliente))
     elements.append(Spacer(1, 12))
 
-    #Primeira tabela
+    #Primeira tabela.
     dados_tabela_1 = [[f'{mes}'], [f'{mes}', f'{quantidade_mes}'], ['Chamados', f'{chamados}'], ['Horas utilizadas', f'{horas_utilizadas_mes}']]
     estilo_tabela_1 = TableStyle([('SPAN', (0, 0), (-1, 0)), ('BACKGROUND', (0, 0), (-1, 0), colors.grey), ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke), ('ALIGN', (0, 0), (-1, -1), 'CENTER'), ('GRID', (0, 0), (-1, -1), 1, colors.black)])
     
     elements.append(criar_tabela(dados_tabela_1, estilo_tabela_1))
     elements.append(Spacer(1, 12))
 
-    #Segunda tabela - Dividida em duas
+    #Segunda tabela - Dividida em duas.
     dados_tabela_2_1 = [['Trimestre'], [f'Mês', f'{mes_1}', f'{mes_2}', f'{mes_3}', 'Tempo Total'], ['Horas', f'{horas_mes_1}', f'{horas_mes_2}', f'{horas_mes_3}', f'{horas_mes_1 + horas_mes_2 + horas_mes_3}']]
     dados_tabela_2_2 = [['Horas Contratadas', f'{horas_contratadas}'], ['Horas utilizadas', f'{horas_utilizadas_trimestre}'], ['Saldo', f'{saldo}'], ['Valor hora adicional', f'R${valor_hora_adicional_trimestre}'], ['Adicional de horas', f'R${adicional_de_horas_trimestre}']]
     estilo_tabela_2_1 = TableStyle([('SPAN', (0, 0), (-1, 0)), ('BACKGROUND', (0, 0), (-1, 0), colors.grey), ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke), ('ALIGN', (0, 0), (-1, -1), 'CENTER'), ('GRID', (0, 0), (-1, -1), 1, colors.black)])
@@ -90,15 +89,15 @@ def gerar_pdf(
     elements.append(criar_tabela(dados_tabela_2_2, estilo_tabela_2_2))
     elements.append(Spacer(1, 12))
 
-    #Terceira tabela
+    #Terceira tabela.
     dados_tabela_3 = [['Faturamento'], ['Valor mensal', f'R${valor_mensal_faturamento}'], ['Serviços adicionais / peças', f'R${serviços_adicionais_ou_peças}'], ['Valor de horas adicionais do trimestre anterior', f'R${valor_de_horas_adicionais_do_trimestre_anterior}'], ['Total geral', f'R${valor_mensal_faturamento + serviços_adicionais_ou_peças + valor_de_horas_adicionais_do_trimestre_anterior}']]
     estilo_tabela_3 = TableStyle([('SPAN', (0, 0), (-1, 0)), ('BACKGROUND', (0, 0), (-1, 0), colors.grey), ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke), ('ALIGN', (0, 0), (-1, -1), 'CENTER'), ('GRID', (0, 0), (-1, -1), 1, colors.black)])
     
     elements.append(criar_tabela(dados_tabela_3, estilo_tabela_3))
     elements.append(Spacer(1, 12))
 
-    #Construir o documento
+    #Criar o documento.
     doc.build(elements)
 
-# Chamando a função para gerar o PDF
-gerar_pdf("arquivo.pdf", mes='Março')
+#Chamando a função para gerar o PDF.
+gerar_pdf('Arquivo.pdf')
